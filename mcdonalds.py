c50b72527fe800394463fb6d1d24ac8605e583be
Wael Al Jishi <wael.aljeshi@ocado.com>
2015-05-23 17:04:05 +0100

    Added end of sequence detection

--- mcdonalds.py
+++ mcdonalds.py
@@ -21,11 +21,25 @@ class McDonalds(object):
 			if self.is_changeable(n):
 				yield n
 
+	def list_non_changeable_smart(self):
+		numbers = count()
+
+		in_a_row = 0
+		in_a_row_required = min(self.denominations)
+
+		for n in numbers:
+			if in_a_row == in_a_row_required:
+					return
+			elif not self.is_changeable(n):
+				in_a_row = 0
+				yield n
+			else:
+				in_a_row += 1
 
 if __name__ == '__main__':
 	mcd = McDonalds((6,9,20))
 
-	for i in mcd.list_non_changeable():
+	for i in mcd.list_non_changeable_smart():
 		print i
 
 
